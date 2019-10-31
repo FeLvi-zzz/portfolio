b187265181840f4bbd3dd8bc2f8430f2b80e4a40
FeLvi-zzz <34182539+FeLvi-zzz@users.noreply.github.com>
2019-10-31 22:56:49 +0900

    lint

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <nav class="navbar is-primary is-fixed-top" role="navigation" aria-label="main navigation">
+    <nav
+      class="navbar is-primary is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
+    >
       <div class="navbar-brand">
         <span role="button" class="navbar-burger" @click="isExpandedNav ^= 1">
           <span aria-hidden="true"></span>
@@ -60,7 +64,11 @@
         <div class="columns">
           <div class="column is-4-desktop is-full-mobile" id="profile">
             <figure class="image is-128x128">
-              <img class="is-rounded" src="./static/felvi.png" alt="felvi's icon" />
+              <img
+                class="is-rounded"
+                src="./static/felvi.png"
+                alt="felvi's icon"
+              />
             </figure>
           </div>
           <div class="column is-8-desktop is-full-mobile">
@@ -100,7 +108,11 @@
                     {{ item.name }}
                   </th>
                   <td align="right">
-                    <span class="icon" v-for="i in item.level" :key="skillKey + i">
+                    <span
+                      class="icon"
+                      v-for="i in item.level"
+                      :key="skillKey + i"
+                    >
                       <i class="fas fa-star"></i>
                     </span>
                     <span
@@ -126,7 +138,11 @@
           Experience
         </h2>
         <div class="columns is-multiline">
-          <div class="column is-half is-full-tablet" v-for="item in experience" :key="item.name">
+          <div
+            class="column is-half is-full-tablet"
+            v-for="item in experience"
+            :key="item.name"
+          >
             <div class="card">
               <div class="card-content has-text-left">
                 <p class="title is-5">
@@ -139,14 +155,19 @@
                 </p>
               </div>
               <div class="tags" v-show="item.tag">
-                <span class="tag" v-for="tag in item.tag" :key="tag">{{ tag }}</span>
+                <span class="tag" v-for="tag in item.tag" :key="tag">{{
+                  tag
+                }}</span>
               </div>
               <div class="card-image" v-show="item.img">
                 <figure class="image">
                   <img :src="item.img" />
                 </figure>
               </div>
-              <div class="card-content has-text-left" :class="{ 'comment-only': !item.img }">
+              <div
+                class="card-content has-text-left"
+                :class="{ 'comment-only': !item.img }"
+              >
                 <p class="comment">{{ item.comment }}</p>
               </div>
             </div>
@@ -215,165 +236,166 @@ export default {
   name: "app",
   created() {
     const pfData = {
-      "about": [
+      about: [
         {
-          "head": "Name",
-          "body": "高田 颯 / Takada Soh"
+          head: "Name",
+          body: "高田 颯 / Takada Soh"
         },
         {
-          "head": "University",
-          "body": "横浜国立大学 経営学部"
+          head: "University",
+          body: "横浜国立大学 経営学部"
         },
         {
-          "head": "Birthday",
-          "body": "1999/03/23"
+          head: "Birthday",
+          body: "1999/03/23"
         },
         {
-          "head": "Hobby",
-          "body": "ギター・アニメ・競プロ"
+          head: "Hobby",
+          body: "ギター・アニメ・競プロ"
         }
       ],
-      "skill": {
+      skill: {
         "言語 / Language": [
           {
-            "name": "JavaScript (ES6+)",
-            "icon": "devicon-javascript-plain",
-            "level": 4
+            name: "JavaScript (ES6+)",
+            icon: "devicon-javascript-plain",
+            level: 4
           },
           {
-            "name": "Python3",
-            "icon": "devicon-python-plain",
-            "level": 3
+            name: "Python3",
+            icon: "devicon-python-plain",
+            level: 3
           },
           {
-            "name": "HTML5",
-            "icon": "devicon-html5-plain",
-            "level": 4
+            name: "HTML5",
+            icon: "devicon-html5-plain",
+            level: 4
           },
           {
-            "name": "CSS3",
-            "icon": "devicon-css3-plain",
-            "level": 4
+            name: "CSS3",
+            icon: "devicon-css3-plain",
+            level: 4
           },
           {
-            "name": "VBA",
-            "icon": "fas fa-file-excel",
-            "level": 3
+            name: "VBA",
+            icon: "fas fa-file-excel",
+            level: 3
           },
           {
-            "name": "GAS",
-            "icon": "devicon-google-plain",
-            "level": 3
+            name: "GAS",
+            icon: "devicon-google-plain",
+            level: 3
           },
           {
-            "name": "Ruby",
-            "icon": "devicon-ruby-plain",
-            "level": 3
+            name: "Ruby",
+            icon: "devicon-ruby-plain",
+            level: 3
           },
           {
-            "name": "Japanese",
-            "level": 4
+            name: "Japanese",
+            level: 4
           },
           {
-            "name": "English",
-            "level": 3
+            name: "English",
+            level: 3
           }
         ],
         "フレームワーク / Framework": [
           {
-            "name": "Node.js",
-            "icon": "devicon-nodejs-plain",
-            "level": 4
+            name: "Node.js",
+            icon: "devicon-nodejs-plain",
+            level: 4
           },
           {
-            "name": "Vue.js",
-            "icon": "devicon-vuejs-plain",
-            "level": 4
+            name: "Vue.js",
+            icon: "devicon-vuejs-plain",
+            level: 4
           },
           {
-            "name": "Nuxt.js",
-            "level": 4
+            name: "Nuxt.js",
+            level: 4
           },
           {
-            "name": "Bulma",
-            "level": 3
+            name: "Bulma",
+            level: 3
           },
           {
-            "name": "jQuery",
-            "icon": "devicon-jquery-plain",
-            "level": 3
+            name: "jQuery",
+            icon: "devicon-jquery-plain",
+            level: 3
           },
           {
-            "name": "Ruby on Rails 5",
-            "icon": "devicon-rails-plain",
-            "level": 3
+            name: "Ruby on Rails 5",
+            icon: "devicon-rails-plain",
+            level: 3
           }
         ],
         "インフラ / Infrastructure": [
           {
-            "name": "AWS",
-            "icon": "devicon-amazonwebservices-original",
-            "level": 3
+            name: "AWS",
+            icon: "devicon-amazonwebservices-original",
+            level: 3
           },
           {
-            "name": "GCP (BigQuery)",
-            "icon": "devicon-google-plain",
-            "level": 2
+            name: "GCP (BigQuery)",
+            icon: "devicon-google-plain",
+            level: 2
           },
           {
-            "name": "FireBase",
-            "icon": "devicon-google-plain",
-            "level": 3
+            name: "FireBase",
+            icon: "devicon-google-plain",
+            level: 3
           },
           {
-            "name": "Router (FutureNet)",
-            "icon": "fas fa-wifi",
-            "level": 2
+            name: "Router (FutureNet)",
+            icon: "fas fa-wifi",
+            level: 2
           },
           {
-            "name": "MySQL",
-            "icon": "devicon-mysql-plain",
-            "level": 3
+            name: "MySQL",
+            icon: "devicon-mysql-plain",
+            level: 3
           },
           {
-            "name": "PostgreSQL",
-            "icon": "devicon-postgresql-plain",
-            "level": 3
+            name: "PostgreSQL",
+            icon: "devicon-postgresql-plain",
+            level: 3
           },
           {
-            "name": "Wireshark",
-            "level": 3
+            name: "Wireshark",
+            level: 3
           }
         ],
         "開発環境 / Environment": [
           {
-            "name": "Windows",
-            "icon": "devicon-windows8-original",
-            "level": 4
+            name: "Windows",
+            icon: "devicon-windows8-original",
+            level: 4
           },
           {
-            "name": "Linux(Ubuntu, CentOS)",
-            "icon": "devicon-linux-plain",
-            "level": 3
+            name: "Linux(Ubuntu, CentOS)",
+            icon: "devicon-linux-plain",
+            level: 3
           },
           {
-            "name": "Visual Studio Code",
-            "level": 4
+            name: "Visual Studio Code",
+            level: 4
           },
           {
-            "name": "Git",
-            "icon": "devicon-git-plain",
-            "level": 3
+            name: "Git",
+            icon: "devicon-git-plain",
+            level: 3
           }
         ]
       },
-      "experience": [
+      experience: [
         {
-          "name": "Literary Forest",
-          "img": require("./static/literaryforest.png"),
-          "comment": "他のユーザーと、ツリー形式でリレー型のアンソロジーをつくるwebサービスです。趣味で長期休暇を使い2週間程度で制作しました。\nリレー型アンソロジーということを意識させるための表現を工夫し、短文小説の書かれたカードを線でつなぐという方法を取りました。",
-          "link": "https://literary-forest.herokuapp.com/",
-          "tag": [
+          name: "Literary Forest",
+          img: require("./static/literaryforest.png"),
+          comment:
+            "他のユーザーと、ツリー形式でリレー型のアンソロジーをつくるwebサービスです。趣味で長期休暇を使い2週間程度で制作しました。\nリレー型アンソロジーということを意識させるための表現を工夫し、短文小説の書かれたカードを線でつなぐという方法を取りました。",
+          link: "https://literary-forest.herokuapp.com/",
+          tag: [
             "Ruby on Rails 5",
             "HTML5",
             "CSS3",
@@ -385,9 +407,10 @@ export default {
           ]
         },
         {
-          "name": "基幹業務システム",
-          "comment": "基幹業務システムの提案・要件定義・設計・開発等を行いました。\nその他社内業務に使用するITシステムの選定、導入・運用作業、お客様に提供するサービスに使用するソフトウェアの選定、プログラムの開発、ネットワークの設計・構築作業などを行いました。",
-          "tag": [
+          name: "基幹業務システム",
+          comment:
+            "基幹業務システムの提案・要件定義・設計・開発等を行いました。\nその他社内業務に使用するITシステムの選定、導入・運用作業、お客様に提供するサービスに使用するソフトウェアの選定、プログラムの開発、ネットワークの設計・構築作業などを行いました。",
+          tag: [
             "Vue.js",
             "Cybozu Kintone",
             "PostgreSQL",
@@ -396,42 +419,38 @@ export default {
           ]
         },
         {
-          "name": "人材管理SaaS開発",
-          "comment": "将来的な拡張性を考えて使用する技術の選定・設計を行い、それを元に開発・テストなどを行いました。",
-          "tag": [
-            "AWS EC2",
-            "AWS S3",
-            "Node.js",
-            "Express",
-            "Nuxt.js"
-          ]
+          name: "人材管理SaaS開発",
+          comment:
+            "将来的な拡張性を考えて使用する技術の選定・設計を行い、それを元に開発・テストなどを行いました。",
+          tag: ["AWS EC2", "AWS S3", "Node.js", "Express", "Nuxt.js"]
         },
         {
-          "name": "その他",
-          "comment": "Paiza Sランク\n横浜国立大学 学業優秀者表彰 平成30年度春学期、秋学期"
+          name: "その他",
+          comment:
+            "Paiza Sランク\n横浜国立大学 学業優秀者表彰 平成30年度春学期、秋学期"
         }
       ],
-      "contact": [
+      contact: [
         {
-          "name": "Google Form",
-          "url": "https://forms.gle/281RFvxPdDzE2tKn7",
-          "icon": "fas fa-envelope"
+          name: "Google Form",
+          url: "https://forms.gle/281RFvxPdDzE2tKn7",
+          icon: "fas fa-envelope"
         },
         {
-          "name": "Twitter",
-          "url": "https://twitter.com/FeLvi_zzz",
-          "icon": "fab fa-twitter"
+          name: "Twitter",
+          url: "https://twitter.com/FeLvi_zzz",
+          icon: "fab fa-twitter"
         },
         {
-          "name": "Github",
-          "url": "https://github.com/felvi-zzz",
-          "icon": "fab fa-github"
+          name: "Github",
+          url: "https://github.com/felvi-zzz",
+          icon: "fab fa-github"
         }
       ]
-    }
+    };
 
-    for(let name in pfData) {
-      this[name] = pfData[name]
+    for (let name in pfData) {
+      this[name] = pfData[name];
     }
   },
   data() {
